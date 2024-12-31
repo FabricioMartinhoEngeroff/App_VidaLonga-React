2c44d08a4ea224a8c90fbc598e0b67f33a6c3d36
Fabricio <fa.engeroff1996@gmail.com>
2024-12-31 10:43:34 -0300

    Adicionado a Pagina Nao encontrada que abre quando há algum erro na aplicação.

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import Favoritos from "./paginas/Favoritos";
+import NaoEncontrada from "./paginas/NaoEncontrada";
 
 const AppRoutes = () => (
   <Routes>
     <Route index element={<Inicio />} />
     <Route path="/favoritos" element={<Favoritos />} />
+    <Route path="*" element={<NaoEncontrada />} />
   </Routes>
 );
 
